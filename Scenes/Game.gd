f43332f4c27c4075af4865e2f6c2854a4d4bff40
berkayyildiz <berkayyildizgm@gmail.com>
2021-02-13 10:37:02 +0300

    Base for background of game scene

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -10,17 +10,20 @@ var people = []
 
 const PERSON = preload("res://Assets/Prefabs/Person.tscn")
 
-var current_part = 1
+var current_part = 0
+
+const BACKGROUNDS = [preload("res://Assets/Images/Backgrounds/background_00.png")]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	change_part()
 	rng.randomize()
 	for i in 3:
 		people.append([])
 		for p in PERSON_COUNT:
 			var person = PERSON.instance()
-			var rand = randomPositonInMoveZone();
+			var rand = random_positon_in_move_zone();
 			person.position.x += rand[0]
 			person.position.y += rand[1]
 			people[i].append(person)
@@ -28,11 +31,13 @@ func _ready():
 	for p in len(people[current_part]):
 		$MoveZone.add_child(people[current_part][p])
 
-func randomPositonInMoveZone():
+func random_positon_in_move_zone():
 	var x = rng.randf_range(-ZONE_X, ZONE_X)
 	var y = rng.randf_range(-ZONE_Y, ZONE_Y)
 	return [x,y]
 
+func change_part ():
+	$Backgound.texture = BACKGROUNDS[current_part]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
