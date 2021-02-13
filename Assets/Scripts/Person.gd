bca61a9969dd6eed281abc751a9a1dc50559ba41
Berkay Ülke <berkayulke99@gmail.com>
2021-02-13 21:12:38 +0300

    trying to add person cat interaction

--- Assets/Scripts/Person.gd
+++ Assets/Scripts/Person.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 const SPEED_Y_REF = [.25,0,0,0,-.25]
+const MAX_CAT_FOLLOWING_DISTANCE = 125
 
 var speed_x = .5
 var speed_y = 0
@@ -8,11 +9,13 @@ var speed_y = 0
 var direction = 1
 var delta_count = 0
 var is_walking = false
+var is_going_to_cat = false
+var is_waiting_cat = false
+var cat: Node2D
+ 
+var movement = Vector2()
 
-var movment = Vector2()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	delta_count = rand_range(-2,2)
 	calc_move()
@@ -20,25 +23,40 @@ func _ready():
 func _process(delta):	
 	delta_count += delta
 	
-	if(delta_count > 3):
+	if delta_count > 3 and not is_going_to_cat and not is_waiting_cat:
 		calc_move()
 			
-	if is_walking:
-		movment.x = speed_x * direction
-		movment.y = speed_y * direction
+	if is_walking and not is_going_to_cat and not is_waiting_cat:
+		movement.x = speed_x * direction
+		movement.y = speed_y * direction
 		var areas = get_overlapping_areas()
 		for area in areas:
 			if "Person" in area.name:
 				collide_with_person(area)
 			else:
 				exclusion_check(area)
-		self.position += movment
+		self.position += movement
 		self.z_index = $BodyShape.position.y
+
+	if not self.cat:
+		return
+
+	var dist_to_cat = position.distance_to(cat.position)
+	if (is_going_to_cat or is_waiting_cat) and dist_to_cat > MAX_CAT_FOLLOWING_DISTANCE:
+		stop_following_cat()
+	if is_going_to_cat:
+		face_to_cat()
+		if dist_to_cat < 55:
+			wait_for_cat()
+		else:
+			go_to_cat()
+		
 		
 func calc_move():
 	direction = rand_dir()
 	delta_count = 0
 	is_walking = rand_bool()
+	speed_x = .5
 	speed_y = SPEED_Y_REF[randi() % SPEED_Y_REF.size()]
 	if is_walking:
 		idle()
@@ -50,10 +68,7 @@ func rand_dir():
 	return pow(-1, randi() % 2);
 	
 func rand_bool():
-	var val = true
-	if randi() % 2:
-		val = false  
-	return val
+	return randi() % 2 == 0
 
 func walk():
 	$AnimatedSprite.play("walk")
@@ -64,28 +79,62 @@ func idle():
 	is_walking = false
 
 func on_hear_meow(cat):
-	print("I can hear cat")
+	is_going_to_cat = true
+	self.cat = cat
+	face_to_cat()
+	
+func go_to_cat():
+	is_going_to_cat = true
+	if is_waiting_cat:
+		is_going_to_cat = false
+		return
+	$AnimatedSprite.play("walk")
+	movement.x = speed_x * direction
+	movement.y = speed_y * direction
+	position += movement
+
+func wait_for_cat():
+	is_waiting_cat = true
+	is_walking = false
+	$AnimatedSprite.play("pet")
+	$AnimatedSprite.stop()
+
+func pet_cat():
+	$AnimatedSprite.play("pet")	
+
+func face_to_cat():
+	if position.x < cat.position.x:
+		direction = 1
+	else:
+		direction = -1
+	self.scale = Vector2(direction, 1)
+
+func stop_following_cat():
+	is_waiting_cat = false
+	is_going_to_cat = false
+	is_walking = true
+	calc_move()
 
 func exclusion_check(area):
 	match area.name:
 		"TopExclusion":
-			movment.y = 0 if movment.y < 0 else movment.y
+			movement.y = 0 if movement.y < 0 else movement.y
 		"BottomExclusion":
-			movment.y = 0 if movment.y > 0 else movment.y
+			movement.y = 0 if movement.y > 0 else movement.y
 		"LeftExclusion":
-			movment.x = 0 if movment.x < 0 else movment.x
+			movement.x = 0 if movement.x < 0 else movement.x
 		"RightExclusion":
-			movment.x = 0 if movment.x > 0 else movment.x
+			movement.x = 0 if movement.x > 0 else movement.x
 
 func collide_with_person(person_area):
 	if global_position.x < person_area.global_position.x:
-		movment.x = 0 if movment.x > 0 else movment.x
+		movement.x = 0 if movement.x > 0 else movement.x
 	if global_position.x > person_area.global_position.x:
-		movment.x = 0 if movment.x < 0 else movment.x
+		movement.x = 0 if movement.x < 0 else movement.x
 	if global_position.y < person_area.global_position.y:
-		movment.y = 0 if movment.y > 0 else movment.y
+		movement.y = 0 if movement.y > 0 else movement.y
 	if global_position.y > person_area.global_position.y:
-		movment.y = 0 if movment.y < 0 else movment.y
+		movement.y = 0 if movement.y < 0 else movement.y
 
 func _on_Person_area_entered(area):
 #	print("Person Area Entered: %s" % area.name)
