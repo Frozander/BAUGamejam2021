18f1abebff87dac4d909537035e8974e77f172bf
berkayyildiz <berkayyildizgm@gmail.com>
2021-02-14 17:57:35 +0300

    fix

--- Assets/Scripts/Game.gd
+++ Assets/Scripts/Game.gd
@@ -176,9 +176,9 @@ func on_person_hit_wall(person):
 #	if not person.is_last_hitted_wall_right and person.part_index == 0:
 #		return
 	if person.is_last_hitted_wall_right:
-		move_person_to_another_part(person,(person.part_index+1) % 3)
+		move_person_to_another_part(person,(person.part_index+1 + 3) % 3)
 	else:
-		move_person_to_another_part(person,(person.part_index-1 )% 3)		
+		move_person_to_another_part(person,(person.part_index-1 + 3 )% 3)		
 		
 func move_person_to_another_part(person,new_part):
 	people[current_part].erase(person)
