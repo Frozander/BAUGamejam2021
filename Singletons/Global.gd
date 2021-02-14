f4980b177cafcbfd18f9d6912aba76b6d79aa363
Berkay Ülke <berkayulke99@gmail.com>
2021-02-14 15:52:23 +0300

    updated take_photo_and_finish_to update flags

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -10,6 +10,8 @@ var pet_meter_step = 10.0
 var pet_meter_current_value = 0.0
 var pet_meter_max_value = 100.0
 
+var is_last_day_photo_taken = false
+
 var ability_cooldown_map = {
 	"meow": Cooldown.new(3.0),
 	"pose": Cooldown.new(40.0),
@@ -36,11 +38,13 @@ func get_current_day_image():
 	return DAY_IMAGE_MAP[current_day]
 
 func take_photo_and_finish_day():
+	is_last_day_photo_taken = true
 	if not taken_image_keys.has(current_day):
 		taken_image_keys.append(current_day)
-	finish_day()
+	get_tree().change_scene("res://Scenes/FadeIn.tscn")
 
 func finish_day():
+	is_last_day_photo_taken = false
 	get_tree().change_scene("res://Scenes/FadeIn.tscn")
 	
 func go_to_next_day():
