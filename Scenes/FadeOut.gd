42de836c11c360d6ee38793dd214fb8dc0f8fd56
Mustafa Batuhan Ceylan <ceylan.mustafabatuhan@gmail.com>
2021-02-14 18:39:37 +0300

    fixed stuff

--- Scenes/FadeOut.gd
+++ Scenes/FadeOut.gd
@@ -4,8 +4,9 @@ export var next_scene = "res://Scenes/DayEnd.tscn"
 
 func _enter_tree():
 	$AnimationPlayer.play("fade_in")
+	$AudioStreamPlayer.play()
 
 
 
-func _on_AnimationPlayer_animation_finished():
+func _on_AudioStreamPlayer_finished():
 	get_tree().change_scene(next_scene)
